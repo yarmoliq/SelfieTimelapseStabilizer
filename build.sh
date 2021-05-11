1ebd41d8e663d9c422e09576b6281bc16b8bc29f
Maxim Yarmolik <yarmoliq@gmail.com>
2021-05-11 19:45:03 +0300

    build folder, project description

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-g++ main.cpp `pkg-config --cflags --libs opencv4`
+
+if [ ! -d "build/" ]
+then
+    mkdir build
+fi
+
+g++ -o build/STS.o main.cpp -O3 `pkg-config --cflags --libs opencv4`
+
 if [ $? -eq 0 ]; then
    echo Successful build!
 fi
