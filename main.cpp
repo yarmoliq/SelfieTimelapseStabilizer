ef25a0cdf2efd8696ffa42f4b83158ade6dade2c
bogomazdmitry <bogomaz.dima2013@gmail.com>
2021-05-18 18:11:19 +0300

    Add adaptive alignment for three chanels

--- main.cpp
+++ main.cpp
@@ -1,26 +1,21 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/core/types.hpp>
-#include <opencv2/objdetect.hpp>
+#include "opencv2/objdetect.hpp"
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/face/facemark.hpp>
 #include <iostream>
 
-#include "visualize/visualize.h"
-#include "exif/exif.h"
-#include "colorCorrection/adaptivePhotoAlignment.h"
+#include "src/colorCorrection/adaptivePhotoAlignment.h"
 
 using namespace cv;
 using namespace face;
 
-const unsigned int OUTPUT_WIDTH  = 3840;
-const unsigned int OUTPUT_HEIGHT = 2160;
-
 int main(int argc, char **argv)
 {
-    Mat img = imread(argv[1], IMREAD_UNCHANGED);
-    
+    Mat img = imread(argv[1]);
+    adaptiveAlignment(img);
     CascadeClassifier face_cascade;
     face_cascade.load("models/haar/frontalface_alt.xml");
 
@@ -38,20 +33,29 @@ int main(int argc, char **argv)
     cl->apply(gray, gray);
 
     std::vector<Rect> faces;
-    face_cascade.detectMultiScale( gray, faces, 1.5, 3, 20, Size(100, 500) );
+    face_cascade.detectMultiScale( gray, faces, 1.1, 3,0, Size(30, 30) );
     
-    std::vector< std::vector<Point2f> > landmarks;
-    if (!facemark->fit(img, faces, landmarks))
+    std::vector< std::vector<Point2f> > shapes;
+    if (!facemark->fit(img, faces, shapes))
     {
         std::cout << "Could not landmark any faces :(\n";
         return 0;
     }
     
-    std::vector<Point2f> lm = landmarks[0];
-    
-    adaptiveAlignment(img);
-    auto output = visualizeLandmarks(img, lm);
-    imwrite("../photos/output.png", output);
+    //color correction
+
+    for (size_t i = 0; i < faces.size(); ++i)
+    {
+        rectangle(img,faces[i], Scalar( 255, 0, 0 ), 4);
+    }
+
+    for (unsigned long i = 0; i < faces.size(); ++i)
+    {
+        for(unsigned long k = 0; k < shapes[i].size(); ++k)
+            circle(img, shapes[i][k], 5, Scalar(0,0,255), 4);
+    }
+
+    imwrite("../photos/output.png", img);
 
     return 0;
 }
